8ae89de6a4c3fe44c965bd9e5c8916f314caa99a
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-14 17:04:32 -0500

    style card shape

--- app/src/main/java/com/example/woof/ui/theme/Shape.kt
+++ app/src/main/java/com/example/woof/ui/theme/Shape.kt
@@ -20,5 +20,6 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp),
     small = RoundedCornerShape(50.dp),
 )
